f1de63fa99140758f94dde59a8fc49afaaf617a3
Yuri Aksenov <aksenov.mailbox@gmail.com>
2018-01-16 12:47:54 +0300

    Service order стили + скрипты

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+/* Service order item hover */
+
+$(".service-order__item button").mouseover(function() {
+  $(this).parent().addClass("service-order__item--active");
+});
+
+$(".service-order__item button").mouseout(function() {
+  $(this).parent().removeClass("service-order__item--active");
+});
+
+/* Service order item height */
+
+$(document).ready(function() {
+  var maxHeight = +$($(".service-order__item h4")[0]).css("height").split("px")[0];
+  for (var i = 1; i < $(".service-order__item h4").length; i++) {
+    if (+$($(".service-order__item h4")[i]).css("height").split("px")[0] > maxHeight) {
+      maxHeight = +$($(".service-order__item h4")[i]).css("height").split("px")[0];
+    }
+  }
+  for (var i = 0; i < $(".service-order__item h4").length; i++) {
+    $($(".service-order__item h4")[i]).css("height", maxHeight);
+  }
+});
