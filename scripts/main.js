61a08e461c72aad2a0dcb2360c969b1e9e424cb3
Yuri Aksenov <aksenov.mailbox@gmail.com>
2018-01-17 13:27:34 +0300

    Offer counter

--- scripts/main.js
+++ scripts/main.js
@@ -33,3 +33,76 @@ $(".close-button").click(function() {
   $(".service-overlay").removeClass("service-overlay--active");
   $(".service-popup").removeClass("service-popup--active");
 });
+
+/* Page scroll */
+
+$(".service-header__menu-item--1").click(function() {
+  $("html, body").animate({
+    scrollTop: $("html").offset().top
+  }, 1000);
+  return false;
+});
+
+$(".service-header__menu-item--2").click(function() {
+  $("html, body").animate({
+    scrollTop: $("#service-person").offset().top
+  }, 1000);
+  return false;
+});
+
+$(".service-header__menu-item--3").click(function() {
+  $("html, body").animate({
+    scrollTop: $("#service-program").offset().top
+  }, 1000);
+  return false;
+});
+
+$(".service-header__menu-item--4").click(function() {
+  $("html, body").animate({
+    scrollTop: $("#service-reviews").offset().top
+  }, 1000);
+  return false;
+});
+
+$(".service-header__menu-item--5").click(function() {
+  $("html, body").animate({
+    scrollTop: $("#service-map").offset().top
+  }, 1000);
+  return false;
+});
+
+/* Offer counter */
+
+function countdownCounter() {
+  var finishDate = new Date(2019, 0, 1);
+  var curentDate = new Date();
+
+  if ((curentDate - finishDate) >= 0) {
+    /*$(".header-top__timer-days span").text(0);
+    $(".header-top__timer-hours span").text(0);*/
+    $(".offer-counter__minutes").text(0);
+    $(".offer-counter__seconds").text(0);
+  } else {
+    var time = finishDate - curentDate;
+    var days = Math.floor(time / (24 * 3600 * 1000));
+    var hours = Math.floor((time / (24 * 3600 * 1000) - days) * 24);
+    var minutes = Math.floor((((time / (24 * 3600 * 1000) - days) * 24) - hours) * 60);
+    var seconds = Math.floor((((((time / (24 * 3600 * 1000) - days) * 24) - hours) * 60) - minutes) * 60);
+
+    /*$(".header-top__timer-days span").text(days);
+    $(".header-top__timer-hours span").text(hours);*/
+    if (minutes < 10) {
+      $(".offer-counter__minutes").text("0" + minutes);
+    } else {
+      $(".offer-counter__minutes").text(minutes);
+    }
+    if (seconds < 10) {
+      $(".offer-counter__seconds").text("0" + seconds);
+    } else {
+      $(".offer-counter__seconds").text(seconds);
+    }
+    
+  }
+};
+
+setInterval(countdownCounter, 1000);
