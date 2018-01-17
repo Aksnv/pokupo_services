283b3baa8d40a1dd9b256976db759fd29b740fc7
Yuri Aksenov <aksenov.mailbox@gmail.com>
2018-01-17 14:03:22 +0300

    Program & reviews carousel

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,7 @@ $(".service-order__item button").mouseout(function() {
   $(this).parent().removeClass("service-order__item--active");
 });
 
+
 /* Service order item height */
 
 $(document).ready(function() {
@@ -22,6 +23,7 @@ $(document).ready(function() {
   }
 });
 
+
 /* Popup form */
 
 $(".button--popup").click(function() {
@@ -33,6 +35,7 @@ $(".close-button").click(function() {
   $(".service-overlay").removeClass("service-overlay--active");
   $(".service-popup").removeClass("service-popup--active");
 });
+
 
 /* Page scroll */
 
@@ -71,6 +74,7 @@ $(".service-header__menu-item--5").click(function() {
   return false;
 });
 
+
 /* Offer counter */
 
 function countdownCounter() {
@@ -101,8 +105,83 @@ function countdownCounter() {
     } else {
       $(".offer-counter__seconds").text(seconds);
     }
-    
   }
 };
 
 setInterval(countdownCounter, 1000);
+
+
+/* Program carousel */
+
+$(".program-button--forward").click(function() {
+  $(".program-button--back").removeAttr("disabled");
+  for (var i = 0; i < $(".service-program__item").length; i++) {
+    if ($($(".service-program__item")[i]).hasClass("service-program__item--active")) {
+      $($(".service-program__item")[i]).removeClass("service-program__item--active");
+      $($(".service-program__item")[i + 1]).addClass("service-program__item--active");
+      if (i == 1) {
+        $(".program-button--forward").attr("disabled", "disabled");
+      }
+      break;
+    }
+  }
+});
+
+$(".program-button--back").click(function() {
+  $(".program-button--forward").removeAttr("disabled");
+  for (var i = 0; i < $(".service-program__item").length; i++) {
+    if ($($(".service-program__item")[i]).hasClass("service-program__item--active")) {
+      $($(".service-program__item")[i]).removeClass("service-program__item--active");
+      $($(".service-program__item")[i - 1]).addClass("service-program__item--active");
+      if (i == 1) {
+        $(".program-button--back").attr("disabled", "disabled");
+      }
+      break;
+    }
+  }
+});
+
+
+/* Reviews carousel */
+
+$(".reviews-button--forward").click(function() {
+  $(".reviews-button--back").removeAttr("disabled");
+  for (var i = 0; i < $(".service-reviews__block").length; i++) {
+    if ($($(".service-reviews__block")[i]).hasClass("service-reviews__block--active")) {
+      $($(".service-reviews__block")[i]).removeClass("service-reviews__block--active");
+      $($(".service-reviews__block")[i + 1]).addClass("service-reviews__block--active");
+      if (i == 1) {
+        $(".reviews-button--forward").attr("disabled", "disabled");
+      }
+      break;
+    }
+  }
+  for (var j = 0; j < $(".service-reviews__pagination-item").length; j++) {
+    if ($($(".service-reviews__pagination-item")[j]).hasClass("service-reviews__pagination-item--active")) {
+      $($(".service-reviews__pagination-item")[j]).removeClass("service-reviews__pagination-item--active");
+      $($(".service-reviews__pagination-item")[j + 1]).addClass("service-reviews__pagination-item--active");
+      break;
+    }
+  }
+});
+
+$(".reviews-button--back").click(function() {
+  $(".reviews-button--forward").removeAttr("disabled");
+  for (var i = 0; i < $(".service-reviews__block").length; i++) {
+    if ($($(".service-reviews__block")[i]).hasClass("service-reviews__block--active")) {
+      $($(".service-reviews__block")[i]).removeClass("service-reviews__block--active");
+      $($(".service-reviews__block")[i - 1]).addClass("service-reviews__block--active");
+      if (i == 1) {
+        $(".reviews-button--back").attr("disabled", "disabled");
+      }
+      break;
+    }
+  }
+  for (var j = 0; j < $(".service-reviews__pagination-item").length; j++) {
+    if ($($(".service-reviews__pagination-item")[j]).hasClass("service-reviews__pagination-item--active")) {
+      $($(".service-reviews__pagination-item")[j]).removeClass("service-reviews__pagination-item--active");
+      $($(".service-reviews__pagination-item")[j - 1]).addClass("service-reviews__pagination-item--active");
+      break;
+    }
+  }
+});
